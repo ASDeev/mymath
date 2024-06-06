794e505fb43219e4cd1711c9bce5ea8f5735fdc0
ASDeev <163474657+ASDeev@users.noreply.github.com>
2024-06-06 16:27:44 +0300

    Update mymath.go

--- mymath.go
+++ mymath.go
@@ -2,26 +2,67 @@ package mymath
 
 import "math"
 
-func Sqrt(x float64) float64 {
-	return math.Sqrt(x)
+// Abs returns the absolute value of x.
+func Abs(x float64) float64 {
+	return math.Abs(x)
 }
 
+// Acos returns the arccosine, in radians, of x.
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+
+// Asin returns the arcsine, in radians, of x.
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+
+// Atan returns the arctangent, in radians, of x.
+func Atan(x float64) float64 {
+	return math.Atan(x)
+}
+
+// Ceil returns the least integer value greater than or equal to x.
 func Ceil(x float64) float64 {
 	return math.Ceil(x)
 }
 
+// Cos returns the cosine of the radian argument x.
+func Cos(x float64) float64 {
+	return math.Cos(x)
+}
+
+// Exp returns e**x, the base-e exponential of x.
+func Exp(x float64) float64 {
+	return math.Exp(x)
+}
+
+// Floor returns the greatest integer value less than or equal to x.
 func Floor(x float64) float64 {
 	return math.Floor(x)
 }
 
+// Log returns the natural logarithm of x.
+func Log(x float64) float64 {
+	return math.Log(x)
+}
+
+// Pow returns x**y, the base-x exponential of y.
 func Pow(x, y float64) float64 {
 	return math.Pow(x, y)
 }
 
-func Max(x, y float64) float64 {
-	return math.Max(x, y)
+// Sin returns the sine of the radian argument x.
+func Sin(x float64) float64 {
+	return math.Sin(x)
 }
 
-func Min(x, y float64) float64 {
-	return math.Min(x, y)
+// Sqrt returns the square root of x.
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+// Tan returns the tangent of the radian argument x.
+func Tan(x float64) float64 {
+	return math.Tan(x)
 }
