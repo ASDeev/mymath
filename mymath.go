adfbd049e8c7f3aa83119d4781689e3e51afa42b
ASDeev <163474657+ASDeev@users.noreply.github.com>
2024-06-06 16:38:38 +0300

    Update mymath.go

--- mymath.go
+++ mymath.go
@@ -2,67 +2,38 @@ package mymath
 
 import "math"
 
-// Abs returns the absolute value of x.
+// Abs возвращает абсолютное значение числа.
 func Abs(x float64) float64 {
-	return math.Abs(x)
+    if x < 0 {
+        return -x
+    }
+    return x
 }
 
-// Acos returns the arccosine, in radians, of x.
-func Cos(x float64) float64 {
-	return math.Acos(x)
+// Max возвращает наибольшее число из двух.
+func Max(x, y float64) float64 {
+    if x > y {
+        return x
+    }
+    return y
 }
 
-// Asin returns the arcsine, in radians, of x.
-func Sin(x float64) float64 {
-	return math.Asin(x)
-}
-
-// Atan returns the arctangent, in radians, of x.
-func Atan(x float64) float64 {
-	return math.Atan(x)
-}
-
-// Ceil returns the least integer value greater than or equal to x.
-func Ceil(x float64) float64 {
-	return math.Ceil(x)
-}
-
-// Cos returns the cosine of the radian argument x.
-func Cos(x float64) float64 {
-	return math.Cos(x)
-}
-
-// Exp returns e**x, the base-e exponential of x.
-func Exp(x float64) float64 {
-	return math.Exp(x)
-}
-
-// Floor returns the greatest integer value less than or equal to x.
-func Floor(x float64) float64 {
-	return math.Floor(x)
-}
-
-// Log returns the natural logarithm of x.
-func Log(x float64) float64 {
-	return math.Log(x)
-}
-
-// Pow returns x**y, the base-x exponential of y.
-func Pow(x, y float64) float64 {
-	return math.Pow(x, y)
-}
-
-// Sin returns the sine of the radian argument x.
-func Sin(x float64) float64 {
-	return math.Sin(x)
-}
-
-// Sqrt returns the square root of x.
+// Sqrt возвращает квадратный корень из числа.
 func Sqrt(x float64) float64 {
-	return math.Sqrt(x)
+    return math.Sqrt(x)
 }
 
-// Tan returns the tangent of the radian argument x.
-func Tan(x float64) float64 {
-	return math.Tan(x)
+// Sin возвращает синус угла в радианах.
+func Sin(x float64) float64 {
+    return math.Sin(x)
+}
+
+// Cos возвращает косинус угла в радианах.
+func Cos(x float64) float64 {
+    return math.Cos(x)
+}
+
+// Yn возвращает натуральное логарифмическое значение Бесселя функции первого рода.
+func Yn(n int, x float64) float64 {
+    return math.Yn(n, x)
 }
